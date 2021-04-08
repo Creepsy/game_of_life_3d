50f12cd45937bf39f8195ad7b757dc970786a994
Creepsy <Robin.gd@outlook.de>
2021-04-08 22:00:42 +0200

    added ability to render meshes

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1,15 +1,29 @@
 #include "mesh.h"
 
-mesh::mesh(const std::vector<float>& vertices, GLenum usage) {
+mesh::mesh(const std::vector<float>& vertices, GLenum usage) : vertices_count(vertices.size()) {
+    glGenVertexArrays(1, &this->vao);
     glGenBuffers(1, &this->vbo);
 
-    glBindBuffer(GL_ARRAY_BUFFER, this->vbo);
+    glBindVertexArray(this->vao);
 
+    glBindBuffer(GL_ARRAY_BUFFER, this->vbo);
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], usage);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+void mesh::load() {
+    glBindVertexArray(this->vao);
+}
+
+void mesh::render() {
+    glDrawArrays(GL_TRIANGLES, 0, this->vertices_count);
 }
 
 mesh::~mesh() {
     glDeleteBuffers(1, &this->vbo);
+    glDeleteVertexArrays(1, &this->vao);
 }
--- src/mesh.h
+++ src/mesh.h
@@ -4,12 +4,17 @@
 #include <GLFW/glfw3.h>
 
 #include <vector>
+#include <cstddef>
 
 class mesh {
     private:
         unsigned int vbo;
+        unsigned int vao;
+        size_t vertices_count;
     public:
         mesh(const std::vector<float>& vertices, GLenum usage);
+        void load();
+        void render();
         ~mesh();
 };
 
