225099c6c8adf89e784daaaa53ca9592b8763eec
Creepsy <Robin.gd@outlook.de>
2021-04-10 22:44:22 +0200

    removed unused uniform

--- shaders/basic_shader.fs
+++ shaders/basic_shader.fs
@@ -4,8 +4,6 @@ in vec3 position;
 
 out vec4 FragColor;
 
-uniform float time;
-
 void main() {
     FragColor = vec4((position.x + 1.0) / 2.0, (position.y + 1.0) / 2.0, (position.z + 1.0) / 2.0, 1.0);
 }
